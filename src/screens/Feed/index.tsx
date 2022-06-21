092ea79f26c9dfe23fb48100ab47bb5c3852318c
DavidGmezHdez <davidgmezhdez@gmail.com>
2022-06-21 22:55:52 +0200

    Merge

--- src/screens/Feed/index.tsx
+++ src/screens/Feed/index.tsx
@@ -3,6 +3,8 @@ import {Button, StyleSheet, View} from 'react-native';
 import {Text} from '@components/Text';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {RootStackParam} from 'navigation/navigation.types';
+import {useDispatch} from 'react-redux';
+import {logout} from '@redux/user/user_actions';
 
 type NavigationStackProp = NativeStackScreenProps<RootStackParam, 'feed'>;
 
@@ -11,10 +13,18 @@ type Props = {
 };
 
 export const Feed = ({navigation}: Props) => {
+  const dispatch = useDispatch<any>();
+
+  const _handleLogout = () => {
+    dispatch(logout());
+    navigation.navigate('login');
+  };
+
   return (
     <View style={styles.container}>
       <Text>Feed</Text>
-      <Button title="Login" onPress={() => navigation.navigate('login')} />
+
+      <Button title="Logout" onPress={_handleLogout} />
     </View>
   );
 };
